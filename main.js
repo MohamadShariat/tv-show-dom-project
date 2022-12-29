afa3b9726ec4c3f0d302eb32705d350acfe09dc4
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2022-12-29 19:10:05 +0330

    end of level 100

--- main.js
+++ main.js
@@ -12,26 +12,47 @@ const axiosFunc = async () => {
     response.data.forEach(item => {
       const card = document.createElement('div');
       card.classList.add('card');
+
       const image = document.createElement('img');
       image.src = item.image.medium;
       card.appendChild(image);
+
       const ul = document.createElement('ul');
       card.appendChild(ul);
+
       const name = document.createElement('li');
       name.textContent = item.name;
       ul.appendChild(name);
+
       const seasonNumber =
         Number(item.season) < 10 ? `0${item.season}` : item.season;
+
       const episodeNumber =
         Number(item.number) < 10 ? `0${item.number}` : item.number;
+
       const episodeCode = document.createElement('li');
       episodeCode.textContent = `S${seasonNumber}E${episodeNumber}`;
       ul.append(episodeCode);
+
+      // const seasonNumberLi = document.createElement('li');
+      // seasonNumberLi.textContent = `Season Number : ${seasonNumber}`;
+      // ul.append(seasonNumberLi);
+
+      // const episodeNumberLi = document.createElement('li');
+      // episodeNumberLi.textContent = `Episode Number : ${episodeNumber}`;
+      // ul.append(episodeNumberLi);
+
       const summary = document.createElement('p');
       summary.innerHTML = `${item.summary.slice(3, -4).slice(0, 100)}...`;
       ul.append(summary);
+
       const anchor = document.createElement('a');
-      anchor.href = 'https://www.tvmaze.com/shows/82/game-of-thrones/episodes';
+      anchor.setAttribute('target', '_blank');
+      anchor.href = `https://www.tvmaze.com/episodes/${
+        item.id
+      }/game-of-thrones-${item.season}x${episodeNumber}-${name.innerHTML
+        .split(' ')
+        .join('-')}`;
       anchor.append(card);
       container.appendChild(anchor);
     });
